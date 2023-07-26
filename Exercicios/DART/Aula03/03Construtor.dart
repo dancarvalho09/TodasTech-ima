f6b91d8973dac704f0b7a0b16e2dffc873fb298a
Danúbia Carvalho <danubiacarvalho.bsk@hotmail.com>
2023-07-26 19:25:37 -0300

    Update

--- Exercicios/DART/Aula03/03Construtor.dart
+++ Exercicios/DART/Aula03/03Construtor.dart
@@ -1,4 +1,6 @@
 // Construtor -> é um método padrão que toda classe e automatiza o resultado. Pode-se ter vários construtores
+// ignore_for_file: unused_local_variable
+
 import 'Pessoa.dart';
 void main() {
   Pessoa p1 = new Pessoa("Isa", "Blumenau");
--- Exercicios/DART/Aula03/04Aluno.dart
+++ Exercicios/DART/Aula03/04Aluno.dart
@@ -1,3 +1,5 @@
+// ignore_for_file: unused_local_variable
+
 import 'Aluno.dart';
 
 void main() {
