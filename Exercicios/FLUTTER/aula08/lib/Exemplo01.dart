fe6d4252b46413cee9a27913945a1ae8e7feb168
Danúbia Carvalho <danubiacarvalho.bsk@hotmail.com>
2023-08-28 19:56:55 -0300

    Update Exercicios

---
+++ Exercicios/FLUTTER/aula08/lib/Exemplo01.dart
@@ -0,0 +1,69 @@
+// Pacote Material
+import 'package:flutter/material.dart';
+
+// StatefulWidget
+class Exemplo01 extends StatefulWidget {
+  const Exemplo01({super.key});
+
+  @override
+  State<StatefulWidget> createState() {
+    return Exemplo01Estrutura();
+  }
+}
+
+class Exemplo01Estrutura extends State {
+  @override
+  Widget build(BuildContext context) {
+
+    // Obter a largura da tela
+    double larguraTela = MediaQuery.of(context).size.width;
+
+    // Tipo de tamanho
+    bool smartphone = false;
+    bool tablet = false;
+    bool desktop = false;
+
+    // Breakpoints
+    if (larguraTela >= 1000){
+      smartphone = false;
+      tablet = false;
+      desktop = true;
+    } else if (larguraTela >= 600 && larguraTela <= 999){
+      smartphone = false;
+      tablet = true;
+      desktop = false;
+    } else {
+      smartphone = true;
+      tablet = false;
+      desktop = true;
+    }
+
+    // Scaffold
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Exemplo 01'),
+      ),
+      body: Center(
+        child: Column(
+          children: [
+            Visibility(visible: smartphone, child: Container(
+                color: Colors.yellow,
+                child: const Text('Telas pequenas - Smartphone', textDirection: TextDirection.ltr,),
+              ),
+            ),
+            Visibility(visible: tablet, child: Container(
+                color: Colors.green,
+                child: const Text('Telas pequenas - Tablet', textDirection: TextDirection.ltr,),
+              ),
+            ),
+            Visibility(visible: desktop, child: Container(
+                color: Colors.red[300],
+                child: const Text('Telas pequenas - Desktop', textDirection: TextDirection.ltr,),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
