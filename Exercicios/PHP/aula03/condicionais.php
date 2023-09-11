56877315a1ed6ac1e7288895b8d32f8b1d37a3b5
Danúbia Carvalho <danubiacarvalho.bsk@hotmail.com>
2023-09-11 20:52:07 -0300

    Class03
    Condicionais e laços

---
+++ Exercicios/PHP/aula03/condicionais.php
@@ -0,0 +1,9 @@
+<?php
+
+$nome = 'Amanda';
+
+if($nome == 'Amanda') {
+    echo 'Olá Amanda';
+} else {
+    echo 'Olá. Quem é você?';
+}
---
+++ Exercicios/PHP/aula03/laco_repeticao.php
@@ -0,0 +1,11 @@
+<?php
+$contador = 1;
+
+while($contador <= 30) {
+    echo "<div>$contador</div>";
+    $contador++;
+}
+echo '<br>';
+for ($cont = 1; $cont <=10; $cont++) {
+    echo $cont . ' ';
+}
